9bc656fccfff2cdf4d1c451c3629c0f44e765c4d
isabelleadrury <isabelledrury38@gmail.com>
2025-07-24 18:11:09 -0400

    added describeTemperature

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function greet ()
-function greet ()
+let describeTemperature = 50;
+
+if (describeTemperature < 0) {
+  ("Very cold");
+} else if (describeTemperature < 20) {
+  ("Cold");
+} else if (describeTemperature < 30) {
+  ("Warm");
+} else if (describeTemperature < 40) {
+  ("Hot");
+} else if (describeTemperature >= 40) {
+  console.log("Very hot");
+}

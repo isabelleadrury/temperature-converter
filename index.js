795bd5c55a6b78eb745393a4bf524dacf02ac3cd
isabelleadrury <isabelledrury38@gmail.com>
2025-07-24 18:47:59 -0400

    added describeTemperature

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-let describeTemperature = 50;
+console.log("Fahrenheit is 86 degrees");
+
+let convertToCelsius = 86;
+
+function subtract(convertToCelsius, num32) {
+  return convertToCelsius - num32;
+}
+
+function multiply(difference, num5) {
+  return difference * num5;
+}
+
+function divide(product, num9) {
+  return product / num9;
+}
+
+const difference = subtract(convertToCelsius, 32);
+const product = multiply(difference, 5);
+const quotient = divide(product, 9);
+console.log("The Temperature is", quotient, "degrees celsius");
+
+let describeTemperature = quotient;
 
 if (describeTemperature < 0) {
-  ("Very cold");
+  console.log("Very cold");
 } else if (describeTemperature < 20) {
-  ("Cold");
+  console.log("Cold");
 } else if (describeTemperature < 30) {
-  ("Warm");
+  console.log("Warm");
 } else if (describeTemperature < 40) {
-  ("Hot");
+  console.log("Hot");
 } else if (describeTemperature >= 40) {
   console.log("Very hot");
 }
